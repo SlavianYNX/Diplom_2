15c576429d7651ab3f725c70e56abd1d70ba09aa
Slava Eremin <slavik.eremin@yandex.ru>
2025-06-29 15:11:48 +0300

    Удалены лишние импорты, тексты ответа сервера вынесены в модули

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,30 +1,31 @@
 import pytest
 import requests
 import allure
+import data
 from data import UrlsApi
-from conftest import create_new_user
 from helpers import UserData
 
 
 class TestCreateUser:
 
+
     @allure.title('Проверка создания уникального пользователя')
     def test_create_user_ok(self, create_new_user):
-        r = create_new_user
-        assert r[1].status_code == 200 and r[1].json()["success"] == True
+        response = create_new_user
+        assert response[1].status_code == data.StatusCode.OK and response[1].json()["success"] == data.TextResponse.TRUE
 
     @allure.title('Проверка создания пользователя, который уже зарегистрированный')
     def test_create_user_two_registered(self, create_new_user):
         response = create_new_user
         payload = response[0]
-        r = requests.post(UrlsApi.CREATE_USER, data=payload)
-        assert r.status_code == 403 and r.json()['message'] == "User already exists"
+        r = requests.post(UrlsApi.CREATE_USER, json=payload)
+        assert r.status_code == data.StatusCode.FORBIDDEN and r.json()['message'] == data.TextResponse.CREATE_DOUBLE_USER
 
     @allure.title('Проверка создания пользователя и незаполнение одного из полей')
     @pytest.mark.parametrize('payload',(UserData.create_user_no_name(),
                                         UserData.create_user_no_password(),
                                         UserData.create_user_no_email()))
     def test_create_no_data(self, payload):
-        r = requests.post(UrlsApi.CREATE_USER, data=payload)
-        assert r.status_code == 403 and r.json()["message"] == "Email, password and name are required fields"
-        assert r.json()["success"] == False
+        r = requests.post(UrlsApi.CREATE_USER, json=payload)
+        assert r.status_code == data.StatusCode.FORBIDDEN and r.json()["message"] == data.TextResponse.NOT_FIELD
+        assert r.json()["success"] == data.TextResponse.FALSE

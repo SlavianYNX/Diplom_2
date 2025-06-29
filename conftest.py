873bad9cb672b006eeb4f77c94cb690f61b4f32c
Slava Eremin <slavik.eremin@yandex.ru>
2025-06-29 15:07:34 +0300

    Удалены лишние импорты, добавлены тексты ответа

--- conftest.py
+++ conftest.py
@@ -1,13 +1,14 @@
 import pytest
 import requests
+
 from data import UrlsApi
 from helpers import UserData
 
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def create_new_user():
     payload = UserData.create_user_data()
-    response = requests.post(UrlsApi.CREATE_USER, data=payload)
+    response = requests.post(UrlsApi.CREATE_USER, json=payload)
     yield payload, response
     token = response.json()["accessToken"]
     requests.delete(UrlsApi.DELETE_USER, headers={'Authorization': token})
